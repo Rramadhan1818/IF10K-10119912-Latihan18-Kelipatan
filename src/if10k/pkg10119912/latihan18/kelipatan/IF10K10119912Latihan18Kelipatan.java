f38d8122ae7d352785f5608c06fac67b66a3ebd2
Rramadhan1818 <55430991+Rramadhan1818@users.noreply.github.com>
2020-10-26 20:28:39 +0700

    first commit

---
+++ src/if10k/pkg10119912/latihan18/kelipatan/IF10K10119912Latihan18Kelipatan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119912.latihan18.kelipatan;
+
+/**
+ *
+ * @author 
+ * NAMA  : Rezki Ramadhan
+ * KELAS : IF10K
+ * NIM  `: 10119912
+ * Deskripsi Program : 
+ * 
+ */
+public class IF10K10119912Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float bil;
+            for(bil=(float) 3.5;bil<=35;bil+=3.5){      
+                System.err.println(bil);               
+            }
+    }
+    
+}
